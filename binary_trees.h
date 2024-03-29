a6fceaa62ae3d20fbcba465618d947964467bf66
SirJupiter <jupitertoby@gmail.com>
2024-03-30 00:09:31 +0100

    17. Sibling

--- binary_trees.h
+++ binary_trees.h
@@ -53,5 +53,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree, int height);
 size_t _binary_tree_height_(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
